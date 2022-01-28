82f356acc105695dd049befa113ddb0085697020
Amadeus Justinn Haryanto <67861264+amadeusjustinn@users.noreply.github.com>
2022-01-28 08:32:43 -0600

    Bring error embed message back, add colour

--- bot/main.py
+++ bot/main.py
@@ -270,7 +270,11 @@ async def on_message(message):
                     data["curr_count"] = 0
                     data["last_user"] = 0
 
-              
+                    embed_m = discord.Embed(colour=0xFFFFFF)
+                    embed_m.add_field(
+                        name="<a:burst2:934223774759399514> 𝗪𝗿𝗼𝗻𝗴 𝗰𝗼𝘂𝗻𝘁 <a:burst2:934223774759399514>",
+                        value=f"𝗹𝗼𝗼𝗸𝘀 𝗹𝗶𝗸𝗲 𝘆𝗼𝘂 𝗺𝗲𝘀𝘀𝗲𝗱 𝘂𝗽 𝘁𝗵𝗲 𝘀𝗲𝗾𝘂𝗲𝗻𝗰𝗲. 𝘁𝗵𝗮𝘁'𝘀 𝗼𝗸𝗮𝘆! 𝘁𝗵𝗲 𝗻𝗲𝘅𝘁 𝗻𝘂𝗺𝗯𝗲𝗿 𝗶𝘀 𝟭 <a:burst4:934223774763581540>")
+                    await message.channel.send(embed=embed_m)              
                 
     # Update JSON file
     with open(filename, "w") as file2:
